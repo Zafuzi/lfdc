a90c72658bbe3645cbc1633c3ca0002fa5d7abe2
Zafuzi <zacharyfoutz@gmail.com>
2017-10-13 22:56:01 -0700

    feat: login now redirects if no user found, if found redirects to times page

--- index.php
+++ index.php
@@ -2,49 +2,37 @@
 require 'bootstrap.php';
 
 // $fire->push("users", array("fname"=>"Monica", "lname" => "Harper", "email"=>"monicawalkswithfaith@yahoo.com", "password"=>"password"));
-// $users = $fire->get("users/*/fname=");
-// foreach($user in $users)
-// printf($username);
+// $users = $fire->get("users/*/fname=Monica");
 
-/**
- * Home pages list
- */
-
+// Home Page
 $app->action('/', function (&$view) {
     global $app;
-    $view  = 'index';
+    $userInfo = $app->getUserInfo();
+    $view = 'index';
+    // $view = 'index';
+    if($userInfo == ''){
+      $vars= array(
+          'title' => 'Home',
+          'userInfo' => $userInfo
+      );
+      return compact('vars');
+    }else{
+      header( 'Location: /times' );
+    }
+});
+
+// Times Page
+$app->action('times', function (&$view) {
+  global $app;
     $vars= array(
-        'title' => 'Home',
-        'userInfo' => $app->getUserInfo(),
-        'links' => array(
-          array('route' => 'login', 'name' => 'The Login page example'),
-          array('route' => 'cities', 'name' => 'The City API search example')
-        )
+        'title' => 'Times',
+        'userInfo' => $app->getUserInfo()
     );
     return compact('vars');
 });
 
-/**
- * Basic user form
- */
-$app->action('login', function () {
-    $username = isset($_POST['username']) ? $_POST['username'] : null;
-    $password = isset($_POST['password']) ? $_POST['password'] : null;
 
-    $message['error']   = !!$_POST;
-    $message['success'] = false;
-
-    if (trim($username) && trim($password)) {
-        $message['error']   = false;
-        $message['success'] = true;
-    }
-
-    return array(
-        'message' => $message,
-        'user'    => compact('username', 'password', 'invalid')
-    );
-});
-
+// Logout
 $app->action('logout', function(){
   global $auth0;
   $auth0->logout();
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,12 +6,81 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit3921f6fe1e67b7f8e78f088bfe95b1fb
 {
+    public static $files = array (
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
+    );
+
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Psr\\Http\\Message\\' => 17,
+        ),
+        'G' => 
+        array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
+        ),
+        'F' => 
+        array (
+            'Firebase\\JWT\\' => 13,
+        ),
+        'D' => 
+        array (
+            'Dotenv\\' => 7,
+        ),
+        'A' => 
+        array (
+            'Auth0\\SDK\\' => 10,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
+        ),
+        'Firebase\\JWT\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
+        ),
+        'Dotenv\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/vlucas/phpdotenv/src',
+        ),
+        'Auth0\\SDK\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/auth0/auth0-php/src',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'P' => 
         array (
             'Pug\\' => 
             array (
                 0 => __DIR__ . '/..' . '/pug-php/pug/src',
+            ),
+        ),
+        'O' => 
+        array (
+            'OAuth2' => 
+            array (
+                0 => __DIR__ . '/..' . '/adoy/oauth2/src',
             ),
         ),
         'N' => 
@@ -32,12 +101,29 @@ class ComposerStaticInit3921f6fe1e67b7f8e78f088bfe95b1fb
                 0 => __DIR__ . '/..' . '/pug-php/pug/src',
             ),
         ),
+        'B' => 
+        array (
+            'Bramus' => 
+            array (
+                0 => __DIR__ . '/..' . '/bramus/router/src',
+            ),
+        ),
+    );
+
+    public static $classMap = array (
+        'Firebase\\Error' => __DIR__ . '/..' . '/ktamas77/firebase-php/src/firebaseStub.php',
+        'Firebase\\FirebaseInterface' => __DIR__ . '/..' . '/ktamas77/firebase-php/src/firebaseInterface.php',
+        'Firebase\\FirebaseLib' => __DIR__ . '/..' . '/ktamas77/firebase-php/src/firebaseLib.php',
+        'Firebase\\FirebaseStub' => __DIR__ . '/..' . '/ktamas77/firebase-php/src/firebaseStub.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit3921f6fe1e67b7f8e78f088bfe95b1fb::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit3921f6fe1e67b7f8e78f088bfe95b1fb::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit3921f6fe1e67b7f8e78f088bfe95b1fb::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit3921f6fe1e67b7f8e78f088bfe95b1fb::$classMap;
 
         }, null, ClassLoader::class);
     }
